b5f9093a9a9a42ad2b4ac62daab7b2e2ca7d4686
krpe <peter.kraus@empa.ch>
2022-05-05 19:19:18 +0200

    Fix parsing 4.1

--- src/dgbowl_schemas/yadg_dataschema/dataschema_4_1/metadata.py
+++ src/dgbowl_schemas/yadg_dataschema/dataschema_4_1/metadata.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel, Extra
-from typing import Optional, Mapping, Literal
+from typing import Optional, Mapping, Literal, Any
 
 
 class Metadata(BaseModel, extra=Extra.forbid):
     class Provenance(BaseModel, extra=Extra.forbid):
         type: str
-        metadata: Optional[Mapping[str, str]]
+        metadata: Optional[Mapping[str, Any]]
 
     provenance: Provenance
     version: Literal["4.1"]
