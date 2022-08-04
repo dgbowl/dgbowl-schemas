911678b60c317fb90e758e50b5edcc5efe9cbb06
krpe <peter.kraus@empa.ch>
2022-08-04 15:15:07 +0200

    Docs version 109

---
+++ 109/_sources/index.rst.txt
@@ -0,0 +1,9 @@
+**dgbowl-schemas**: schemas and validators for the dgbowl suite of tools
+========================================================================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: dgbowl-schemas autodocs
+   :hidden:
+
+   dgbowl_schemas
--- versions.js
+++ versions.js
@@ -4,6 +4,7 @@ document.write('\
 <dl>\
     <dt>Versions</dt> \
     <dd><a href="../master/' + page + '">master</a></dd>\
+    <dd><a href="../109/' + page + '">109</a></dd>\
     <dd><a href="../106/' + page + '">106</a></dd>\
     <dd><a href="../104/' + page + '">104</a></dd>\
     <dd><a href="../103/' + page + '">103</a></dd>\
