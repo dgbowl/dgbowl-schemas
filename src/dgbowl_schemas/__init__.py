43af0b879691980c1f9386f9acac772232b27868
Peter Kraus <peter.kraus@tu-berlin.de>
2023-01-05 07:36:41 +0100

    Silence linter.

--- src/dgbowl_schemas/__init__.py
+++ src/dgbowl_schemas/__init__.py
@@ -1,3 +1,3 @@
-from .dgpost import Recipe, to_recipe
-from .tomato import Payload, to_payload
-from .yadg import DataSchema, to_dataschema
+from .dgpost import Recipe as Recipe, to_recipe as to_recipe
+from .tomato import Payload as Payload, to_payload as to_payload
+from .yadg import DataSchema as DataSchema, to_dataschema as to_dataschema
