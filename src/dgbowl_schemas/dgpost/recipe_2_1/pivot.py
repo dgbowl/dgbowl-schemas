f516cdb630d43e8f9d464ef7ec4fc9555df81d9e
Peter Kraus <peter.kraus@tu-berlin.de>
2023-06-23 13:40:24 +0200

    Add "as" and move "on" to "using"

--- src/dgbowl_schemas/dgpost/recipe_2_1/pivot.py
+++ src/dgbowl_schemas/dgpost/recipe_2_1/pivot.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel, Extra
+from pydantic import BaseModel, Extra, Field
 from typing import Sequence, Literal
 
 
 class Pivot(BaseModel, extra=Extra.forbid, allow_population_by_field_name=True):
     table: str
-    on: str
+    as_: str = Field(alias="as")
+    using: str
     columns: Sequence[str] = None
     timestamp: Literal["first", "last", "mean"] = "first"
