24f23ac1f14cffe1b898e1c67c0e566cea60b312
PeterKraus <PeterKraus@users.noreply.github.com>
2025-10-24 17:22:02 +0000

    deploy: d153e57843b679ec41f82d913678f307cf4f590f

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '124.dev1',
+    VERSION: '124.dev2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
